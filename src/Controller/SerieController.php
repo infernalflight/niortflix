b350dd9dd8a984e96a3fc1e3d3f98bfad22b899d
ffotre <ffotre@campus-eni.fr>
2024-04-17 12:08:39 +0200

    1st commit

---
+++ src/Controller/SerieController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class SerieController extends AbstractController
+{
+    #[Route('/serie/create', name: 'app_serie_create')]
+    public function create(EntityManagerInterface $em): Response
+    {
+
+        $serie = new Serie();
+        $serie->setName('The Witcher');
+        $serie->setOverview("Ciri & Geralt vivent de formidables aventures...");
+        $serie->setFirstAirDate(new \DateTime('2019-04-14'));
+        $serie->setStatus('Returning');
+        $serie->setDateCreated(new \DateTime());
+
+        $em->persist($serie);
+        $em->flush();
+
+        return new Response('Une serie a été crée');
+    }
+
+    #[Route('/serie/list', name: 'app_serie_list')]
+    public function index(SerieRepository $serieRepository): Response
+    {
+        $series = $serieRepository->findAll();
+
+        return $this->render('serie/index.html.twig', [
+            'series' => $series
+        ]);
+    }
+
+    #[Route('/serie/details/{id}', name: 'app_serie_details', requirements: ['id' => '\d+'])]
+    public function details(int $id, SerieRepository $serieRepository): Response
+    {
+        $serie = $serieRepository->find($id);
+
+        return $this->render('serie/details.html.twig', [
+            'serie' => $serie
+        ]);
+    }
+
+
+}
