bbd937a0708cd3f1344f2f51eb487ba8706308b1
ffotre <ffotre@campus-eni.fr>
2024-04-18 14:33:08 +0200

    Pagination de la liste

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -7,6 +7,7 @@ use App\Repository\SerieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
 
 class SerieController extends AbstractController
@@ -27,12 +28,22 @@ class SerieController extends AbstractController
         return new Response('Une serie a été crée');
     }
 
-    #[Route('/serie/list', name: 'app_serie_list')]
-    public function index(SerieRepository $serieRepository): Response
+    #[Route('/serie/list/{page}', name: 'app_serie_list', requirements: ['page' => '\d+'], defaults: ['page' => 1])]
+    public function index(SerieRepository $serieRepository, int $page): Response
     {
+        if ($page < 1) {
+            throw new NotFoundHttpException('Page non valable');
+        }
 
         // Grosse maille : la méthode héritée
-        $series = $serieRepository->findAll();
+        //$series = $serieRepository->findAll();
+
+        $offset = ($page - 1) * 18;
+
+        $series = $serieRepository->findBy([], ['popularity' => 'DESC'], 18, $offset);
+        $nbSeries = $serieRepository->count();
+
+        $maxPages = ceil($nbSeries / 18);
 
         // Plus fin : le queryBuilder
         //$series = $serieRepository->findBySeveralCriterias('SF', 'Comedy', (new \DateTime('-3 year'))->format('Y-m-d'));
@@ -45,20 +56,28 @@ class SerieController extends AbstractController
 
 
         return $this->render('serie/index.html.twig', [
-            'series' => $series
+            'series' => $series,
+            'page' => $page,
+            'max_pages' => $maxPages,
         ]);
     }
 
-    #[Route('/serie/by_status/{status}', name: 'app_serie_list_by_status', requirements: ['status' => 'Returning|Canceled|Ended'])]
-    public function listByStatus(SerieRepository $serieRepository, string $status): Response
+    #[Route('/serie/by_status/{status}/{page}', name: 'app_serie_list_by_status', requirements: ['status' => 'Returning|Canceled|Ended', 'page' => '\d+'], defaults: ['page' => 1])]
+    public function listByStatus(SerieRepository $serieRepository, string $status, int $page): Response
     {
-        $series = $serieRepository->findBy(['status' => $status], ['firstAirDate' => 'DESC']);
+        $offset = ($page - 1) * 18;
+
+        $series = $serieRepository->findBy(['status' => $status], ['firstAirDate' => 'DESC'], 18, $offset);
+        $nbSeries = $serieRepository->count(['status' => $status]);
+
+        $maxPages = ceil($nbSeries / 18);
 
         return $this->render('serie/index.html.twig', [
-            'series' => $series
+            'series' => $series,
+            'page' => $page,
+            'max_pages' => $maxPages,
         ]);
     }
-
 
     #[Route('/serie/details/{id}', name: 'app_serie_details', requirements: ['id' => '\d+'])]
     public function details(int $id, SerieRepository $serieRepository): Response
