86dea309fffd25ba89bc33365c967697e99f5c49
ffotre <ffotre@campus-eni.fr>
2024-04-24 12:21:01 +0200

    Contrainte unicité sur category.nom

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,8 +6,10 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[UniqueEntity('name', message: 'Ce nom existe déja')]
 class Category
 {
     #[ORM\Id]
@@ -15,7 +17,7 @@ class Category
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $name = null;
 
     /**
