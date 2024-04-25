00502fa84e3264e57012cdf23468283e1c938e18
ffotre <ffotre@campus-eni.fr>
2024-04-25 11:36:41 +0200

    display user.login in navbar

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -12,7 +12,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
